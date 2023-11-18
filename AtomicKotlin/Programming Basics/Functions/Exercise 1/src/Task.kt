38062e40076e6406830e0ff0fd976f2fdfc55677
Gorthdzx <krislaksana123@gmail.com>
2023-11-18 18:28:29 +0700

    function  commit

--- AtomicKotlin/Programming Basics/Functions/Exercise 1/src/Task.kt
+++ AtomicKotlin/Programming Basics/Functions/Exercise 1/src/Task.kt
@@ -1,8 +1,8 @@
 // Functions/Task1.kt
 package functionsExercise1
 
-fun squareInt(i: Int): Int = i * 2
+fun squareInt(i: Int): Int = i * i
 
 fun main() {
-  println(squareInt(2))  // 4
+  println(squareInt(5))  // 4
 }
